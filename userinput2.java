6f813bfb7c313138caed7cb75b048f2a6e2b6a96
padmaja0793 <padmaja7210@gmail.com>
2025-12-11 11:23:16 +0530

    Java Practice

---
+++ userinput2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class userinput2 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+    
+}

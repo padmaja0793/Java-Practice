0dbcb7de632437a2ff7575564629093420ff9347
padmaja0793 <padmaja7210@gmail.com>
2025-12-16 22:09:44 +0530

    Java Practice

---
+++ Slength.java
@@ -0,0 +1,9 @@
+public class Slength {
+    public static void main(String args[]){
+
+        String str ="Padmaja";
+        int length = str.length();
+        System.out.println("String length" + length);
+    }
+    
+}
